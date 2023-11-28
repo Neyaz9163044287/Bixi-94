da9f7b6b5db9aba8de46e9928915f5ab58206dff
Neyaz <Neyaz@Neyaz-PC>
2023-11-28 12:17:06 +0530

    Test script 2

---
+++ src/test/java/com/Testyantra/Bixi/TestScriptTest2.java
@@ -0,0 +1,21 @@
+package com.Testyantra.Bixi;
+
+import org.testng.annotations.Test;
+
+public class TestScriptTest2 {
+
+	public class TestScriptTest 
+	{
+		@Test(groups = "smoke")
+		public void ts3Test()
+		{
+			System.out.println("--TestScript3--");
+		}
+		
+		@Test(groups = "regression")
+		public void ts4Test()
+		{
+			System.out.println("--TestScript4--");
+		}
+	}
+}
